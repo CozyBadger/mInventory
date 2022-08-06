3a991bcfbcf48fbb10e079ce3f97353af422cf79
CozyBadger <martin@pagel.cloud>
2022-08-06 22:33:33 +0200

    adjusting reqs and changing core html

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template
-
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
 
 app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 
 
 @app.route("/")
